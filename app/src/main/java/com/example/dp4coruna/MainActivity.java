b60ad7a933f410b14dc4d7213ea2e98c544ea5eb
Rizwan Chowdhury <rizwanchowdhury111@gmail.com>
2020-07-28 17:19:05 -0400

    added location object

--- app/src/main/java/com/example/dp4coruna/MainActivity.java
+++ app/src/main/java/com/example/dp4coruna/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.dp4coruna;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int ACCESS_FINE_LOCATION_REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkForPermissions(getApplicationContext());
+    }
+
+
+    /**
+     * Checks for permission at the start of the app.
+     * Currently permission being chekced is:
+     *      - Access Location Coarse
+     *
+     */
+    private void checkForPermissions(Context context){
+        if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                                ACCESS_FINE_LOCATION_REQUEST_CODE);
+        }
     }
 }
--- app/src/main/java/com/example/dp4coruna/SensorReader.java
+++ app/src/main/java/com/example/dp4coruna/SensorReader.java
@@ -1,9 +1,14 @@
 package com.example.dp4coruna;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.telephony.*;
+import android.util.Log;
+import androidx.core.content.ContextCompat;
 
 import java.util.Collections;
 import java.util.List;
@@ -13,6 +18,7 @@ import java.util.List;
  */
 public class SensorReader {
 
+    private static final String LOG_CAT_TAG = "SensorReader";
     /**
      * Gets list of current wifi access points
      * @param context current device context required to obtain the wifi-access point data
@@ -30,10 +36,35 @@ public class SensorReader {
     }
 
     public static void getCellInfoAtMoment(Context context){
-        if(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY)){
+        if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED){
 
+            if(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY)){
+                TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+                List<CellInfo> cellInfoList = tm.getAllCellInfo();
+                getCellInfoFeatures(cellInfoList);
+            }
         }
 
+    }
+
+    private static void getCellInfoFeatures(List<CellInfo> cellInfoList){
+        for(CellInfo element : cellInfoList){
+            String toBeLogged = "";
+
+            if(element instanceof CellInfoGsm){
+                CellIdentityGsm gsmi = ((CellInfoGsm) element).getCellIdentity();
+                toBeLogged = String.valueOf(gsmi.getLac());
+                toBeLogged += " " + String.valueOf(((CellInfoGsm) element).getCellSignalStrength().getLevel());
+            }
+            else if(element instanceof CellInfoLte){
+                CellIdentityLte ltei = ((CellInfoLte) element).getCellIdentity();
+                toBeLogged = String.valueOf(ltei.getTac());
+                toBeLogged += " " + String.valueOf(((CellInfoLte) element).getCellSignalStrength().getLevel());
+            }
+
+            Log.i(LOG_CAT_TAG,toBeLogged);
+        }
     }
 
 }
