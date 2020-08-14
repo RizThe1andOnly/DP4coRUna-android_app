3930eba1331aa797cbd86ac9894ecc527858a33a
Rizwan <risla156@gmail.com>
2020-08-14 01:12:24 -0400

    ml model is functioning now

--- app/src/main/java/com/example/dp4coruna/ml/MLModel.java
+++ app/src/main/java/com/example/dp4coruna/ml/MLModel.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import com.example.dp4coruna.R;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
@@ -34,6 +35,25 @@ import java.util.List;
  *  - Database code
  *  - Whole app
  * For now will be working with this as a standalone class which is not dependent on any other class in this app
+ *
+ * Currently this section uses random data that is stored in app->res->raw->dp4sampledata.csv
+ *
+ *
+ *      --How the data needs to be formatted to be used in the model (as i know so far)--
+ *  DeepLearning4J (DL4J) provides its own classes for machine learning and for holding data, so we need to setup data
+ *  in a way to utilize them.
+ *
+ *  - The model in DL4J takes in a DataSet object for the fit method.
+ *      - The DataSet object takes in NDArray objects as parameters, one for labels(will mention below) and one for
+ *        features.
+ *          - NDArray is the DL4J analogue of a numpy array and takes in arrays as parameters for constructor and builds
+ *            an NDArray object.
+ *
+ *  - Note this for the labels: They essentially need to be in one-hot-encoding format. This will be achieved through
+ *                              creating an array for each label and having all zeros except for one element which will
+ *                              be '1' corresponding to the label. It will be a matrix with rows and columns equal to
+ *                              number of labels/samples. Each row will represent a label and the column with column
+ *                              index matching the row index will be value '1' otherwise '0'.
  */
 public class MLModel {
 
@@ -56,6 +76,9 @@ public class MLModel {
 
     /**
      * Will read provided data (from csv for now) and format it to be used by machine learning model
+     *
+     * This is the random data that was generated before, this is only for testing purposes. We will eventually
+     * obtain data from the database and then process them.
      */
     public void obtainDataSet(Activity activity){
         CSVReader cr = new CSVReader(new InputStreamReader(activity.getResources().openRawResource(R.raw.dl4jsampledata)));
@@ -92,18 +115,24 @@ public class MLModel {
                 featuresMatrix[i][j] = dataMatrix[i][j+1];
             }
         }
-        float[] labelArray = new float[dataMatrix.length];
+
+        // one-hot-encoding the labels (usually we would have to create a mapping between these and labels but since
+        // this is all random we won't do it until later)
+        float[][] labelArray = new float[dataMatrix.length][dataMatrix.length];
         for(int i=0;i< dataMatrix.length;i++){
-            labelArray[i] = dataMatrix[i][0];
+            labelArray[i][i] = 1;
         }
 
-        this.trainingData = new DataSet(new NDArray(labelArray),new NDArray(featuresMatrix));
-
+        this.trainingData = new DataSet(new NDArray(featuresMatrix),new NDArray(labelArray));
+        //Log.i("FromMLModel",this.trainingData.toString());
     }
 
 
     /**
-     * Convert matrix of string data into a matrix of float data
+     * Convert matrix of string data into a matrix of float data.
+     *
+     * Currently used by the obtain data testing method. (!!!)
+     *
      * @param stringData
      * @return float[][]
      */
@@ -122,19 +151,22 @@ public class MLModel {
         return matrix;
     }
 
-    /**
-     * Will convert provided string into a sample/row that can be added to Dataset to be processed by
-     * machine learning model.
-     *
-     * @param rowString features for a single sample in string format
-     */
-    private void createRow(String[] rowString){
-
-    }
-
 
     /**
      * Will create and adjust a multinomial logistic regression (softmax) model.
+     *
+     * (!!!) Core component of our current machine learning model. It uses the the DeepLearning4J's MultiLayerConfiguration
+     * and MultiLayerNetwork objects for the machine learning.
+     *
+     * The three variables under "//vars to be used in model adjustments" may need to be adjusted based on algo. Aside
+     * from that at the moment (unless there are changes in number of features) there are no need for changes here.
+     *
+     * Note the for loop that call model.fit() on this model, this is the training for the model with data on location
+     * features available in the device database. The for loop has the condition of the epochs, so that needs to be
+     * adjusted based on how many epoch we want for this data.
+     *
+     *  --Look at top of class for description on how data needs to be setup--
+     *
      */
     public void createMlModel(){
         //vars to be used in model adjustments:
@@ -145,15 +177,20 @@ public class MLModel {
         //create model configurations then initialize model with configuration:
         MultiLayerConfiguration config = new NeuralNetConfiguration.Builder()
                 .seed(seed)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
                 .updater(new Nesterovs(learningRate,0.9))
                 .list()
                 .layer(new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .nIn(NUMBER_OF_FEATURES-1)
                         .activation(Activation.SOFTMAX)
-                        .nOut(NUMBER_OF_FEATURES).build())
+                        .nOut(9).build())
                 .build();
 
         this.mln = new MultiLayerNetwork(config);
+
+
         this.mln.init();
+
 
         /**
          * Fit model to data below.
